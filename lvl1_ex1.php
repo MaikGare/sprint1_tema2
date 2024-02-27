ee0e686177ddd059c918d51b671ec337fdf95f9d
MaikGare <155468145+MaikGare@users.noreply.github.com>
2024-02-27 16:17:40 +0100

    Add files via upload

---
+++ lvl1_ex1.php
@@ -0,0 +1,14 @@
+<?php 
+    $int = 5;
+    $double = 3.1416;
+    $string = "Goodbye Lenny";
+    $boo = true;
+
+    echo $int . "<br>" . $double . "<br>" . $string . "<br>" . $boo . "<br>"; 
+
+    define("nombre","miquel garcia revaliente");
+
+    $titulo = ucwords(nombre);
+
+    echo $titulo;
+?>
